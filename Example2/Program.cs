f33d3caa6bce955adf1497759853173032ac3514
«Aleksander <pavel4uk18@gmail.com>
2022-05-18 18:29:32 +0300

    example2

---
+++ Example2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных точек в этой плоскости
+// сначала получить число - > проверить 1/2/3/4 -> выдать диапазон четверти
+int GetQuarter()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите номер четверти, диапазон которой хотите узнать");
+        int quater = Convert.ToInt32(Console.ReadLine());
+        if (quater != 1 && quater != 2 && quater != 3 && quater != 4)
+        {
+            Console.WriteLine("Введите число от 1 до 4");
+        }
+        else
+            return quater;
+    }
+}
+
+void GetRange(int quater) // функция принимает выходной параметр
+{
+    if(quater ==1)
+    {
+        Console.WriteLine("х > 0, y > 0");
+    }
+    else if (quater == 2)
+    {
+        Console.WriteLine ("x < 0, y > 0");
+    }
+    else if (quater == 3)
+    {
+        Console.WriteLine ("x < 0, y < 0");
+    }
+    else
+    {
+        Console.WriteLine (" x > 0, y < 0");
+    }
+}
+
+int giveQuater = GetQuarter();
+GetRange(giveQuater);
