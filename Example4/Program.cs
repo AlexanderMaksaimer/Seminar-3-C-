84b49b84d4fed939e72fa0194554bd64060d31d6
«Aleksander <pavel4uk18@gmail.com>
2022-05-18 19:36:26 +0300

    example4

---
+++ Example4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход число (N) и
+// выдает таблицу квадратов чисел от 1 до N
+
+void GetSquar(int N)
+{
+    foreach (int ind in Enumerable.Range(1, N))
+    //foreach (int ind in Range(1,N))
+    System.Console.Write($"{ind * ind}, ");
+}
+
+//функция которая получает число, введенное с клавиатуры
+//+ проверка на то что ввели именно число
+//+ проверка что число !=0
+int GetNumber(string msg)
+{
+    
+    while(true)
+    {
+        Console.WriteLine(msg);
+        string valueFromConsole = Console.ReadLine();
+
+        if(int.TryParse(valueFromConsole, out int number))
+        {
+            if(number > 0)
+                return number;
+            else
+            {
+                Console.WriteLine("Число должно быть больше нуля");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Нужно ввести число отличное от нуля.");
+        }
+    }
+}
+
+string messageI = "Введите N от 1, N =";
+int sqr = GetNumber(messageI);
+
+GetSquar(sqr);
